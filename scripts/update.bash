0627e879d6c6965c6f89da5dfed9070251a83dbb
Charlie Vieth <cvieth@lyft.com>
2020-11-17 12:16:05 -0500

    WIP: update

--- scripts/update.bash
+++ scripts/update.bash
@@ -36,49 +36,46 @@ fix_import_paths() {
     return 0
 }
 
+disable_gopls_main() {
+    fix_import_paths 'package main // import "golang.org/x/tools/gopls"' 'package main'
+    local src
+    src="$(cat ./gopls/main.go)"
+    disable='func init() { panic("WRONG GOPLS") }'
+    printf '// +build never\n\n%s\n\n%s\n' "${src}" "${disable}" > ./gopls/main.go
+    gofmt -w ./gopls/main.go
+    git add ./gopls/main.go
+}
+
 git fetch --all
 
 LATEST_SHA="$(git rev-parse golang/master)"
 BRANCH="golang-$(head -c 8 <<<"$LATEST_SHA")"
-
-# if git branch | grep -F "$BRANCH"; then
-#     git branch -D "$BRANCH"
-# fi
-# git checkout -b "$BRANCH"
 
 git checkout golang/master internal
 git restore --staged ./internal
 
 fix_import_paths golang.org/x/tools/internal github.com/charlievieth/xtools ./internal
 
-# PACKAGES=()
-
 for file in ./internal/*; do
     dest="./$(basename "$file")"
     mv "$file" "$dest"
     git add "$dest"
-    # if [[ -d $dest ]]; then
-    #     PACKAGES+=("$dest")
-    # fi
 done
 rm -r ./internal
 
-# for pkg in "${PACKAGES[@]}"; do
-#     fix_import_paths "golang.org/x/tools/internal/$pkg" "github.com/charlievieth/xtools/$pkg"
-# done
-
 git checkout golang/master gopls
 git restore --staged ./gopls
+
+# remove gopls mod file
 rm ./gopls/go.{mod,sum}
+
+# disable the gopls main (so we don't install it by accident)
+disable_gopls_main
 
 fix_import_paths golang.org/x/tools/internal github.com/charlievieth/xtools ./gopls
 fix_import_paths golang.org/x/tools/gopls/internal github.com/charlievieth/xtools/gopls ./gopls
 fix_import_paths golang.org/x/tools/gopls github.com/charlievieth/xtools/gopls ./gopls
 
-# Fix main.go
-fix_import_paths 'package main // import "golang.org/x/tools/gopls"' 'package main' ./gopls
-MAIN="$(cat ./gopls/main.go)"
-printf '// +build never\n\n%s' "$MAIN" > ./gopls/main.go
 
 for file in ./gopls/internal/*; do
     dest="./gopls/$(basename "$file")"
@@ -89,7 +86,7 @@ rm -r ./gopls/internal
 
 git add ./gopls
 
-# GO111MODULE=on go mod tidy
-# git add go.mod go.sum
+GO111MODULE=on go mod tidy
+git add go.mod go.sum
 
 git commit -m "update golang.org/x/tools to $(head -c 8 <<<"$LATEST_SHA")"
