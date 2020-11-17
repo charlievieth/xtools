525c38fc41d08d57d1b6688131f26b264ed54a5f
Charlie Vieth <cvieth@lyft.com>
2020-11-17 11:50:53 -0500

    WIP: update

--- scripts/update.bash
+++ scripts/update.bash
@@ -8,9 +8,67 @@ FROM=golang.org/x/tools
 PKG=github.com/charlievieth/xtools
 TO="$PKG"
 
+GREP_FLAGS=(
+    --recursive
+    --exclude-dir 'scripts'
+    --exclude-dir '.git'
+    --files-with-matches
+    --null
+    --fixed-strings
+)
+
+escape_import_path() {
+    sed -e 's/\./\\\./g' -e 's/\//\\\//g' <<<"$1"
+}
+
+fix_import_paths() {
+    local from="$1"
+    local to="$2"
+    local dir="${3:"$ROOT"}"
+
+    local replace
+    replace="s/$(escape_import_path "$from")/$(escape_import_path "$to")/g"
+
+    if ! grep "${GREP_FLAGS[@]}" "$from" "$dir" | xargs -0 -- sed -i "$replace"; then
+        echo "error: fix_import_paths" >&2
+        return 1
+    fi
+    return 0
+}
+
 git fetch --all
-git checkout origin/golang internal
+git checkout golang/master internal
+git restore --staged ./internal
+
+fix_import_paths golang.org/x/tools/internal github.com/charlievieth/xtools ./internal
+
+# PACKAGES=()
 
 for file in ./internal/*; do
-    mv "$file" "./$(basename "$file")"
+    dest="./$(basename "$file")"
+    mv "$file" "$dest"
+    git add "$dest"
+    # if [[ -d $dest ]]; then
+    #     PACKAGES+=("$dest")
+    # fi
 done
+rm -r ./internal
+
+# for pkg in "${PACKAGES[@]}"; do
+#     fix_import_paths "golang.org/x/tools/internal/$pkg" "github.com/charlievieth/xtools/$pkg"
+# done
+
+git checkout golang/master gopls
+git restore --staged ./gopls
+
+fix_import_paths golang.org/x/tools/internal github.com/charlievieth/xtools ./gopls
+fix_import_paths golang.org/x/tools/gopls/internal github.com/charlievieth/xtools/gopls ./gopls
+
+for file in ./gopls/internal/*; do
+    dest="./gopls/$(basename "$file")"
+    mv "$file" "$dest"
+    git add "$dest"
+done
+rm -r ./gopls/internal
+
+git add ./gopls
